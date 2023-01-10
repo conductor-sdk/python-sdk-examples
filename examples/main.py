779eb75a48e5e556ebc645a2550ca761149fa87e
gardusig <gustavo.gardusi@gmail.com>
2023-01-09 23:13:46 -0300

    Updated main

--- examples/main.py
+++ examples/main.py
@@ -1,7 +1,10 @@
 import sys
 sys.path.insert(1, '../')
 
-from examples.worker.worker_util import start_workers
+from examples.worker import worker_util
+from examples.workflow import workflow_util
 
-task_handler = start_workers()
+task_handler = worker_util.start_workers()
+workflow_util.start_workflow_sync()
+workflow_util.start_workflow_async()
 task_handler.stop_processes()
